98224211f7fad0d6001082ab5865c2238b3854f3
Ale-Shadow-02 <54350784+Ale-Shadow-02@users.noreply.github.com>
2019-08-22 12:00:25 +0400

    Домашнее задание урок № 1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let money = 20000;
+let income = 'Hello';
+let addExpenses = money;
+let deposit = '100%';
+let mission = 'pass diploma';
+let period = 'autumn';
+
+alert('Поздравляю с началом курса!');
+
+console.log('Желаю всем достичь желаемого на курсе');
+console.log(deposit);
+console.log(mission);
