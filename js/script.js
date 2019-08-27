892f64191cbceec9b049095b614c3e4ce371a90a
Alexandr_Dvoryadkin <dvoryadkin66@mail.ru>
2019-08-27 11:25:24 +0400

    Доработка по кометам куратора к 3 уроку

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?'); // 1)
+let money = +prompt('Ваш месячный доход?'); // 1)
+
+if (!money || 0 === money.lenght) {
+  alert('Перзагрузите страницу и Введите сумму цифрами ');
+}
 
 let addExpenses = prompt('Перечислете возможные расходы за рссчитываемый период через запятую');
 console.log('addExpenses: ', addExpenses.split(',')); // 2)
@@ -9,22 +13,27 @@ let deposit = confirm('Есть ли у вас депозит в банке?'); 
 
 console.log(typeof money);
 console.log(typeof addExpenses);
-console.log(typeof deposit);    // 4)
+console.log(typeof deposit); // 4)
 
 let expenseArr1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
 let sum1 = prompt('Во сколько это обойдется?');
 
 let expenseArr2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-let sum2 = prompt('Во сколько это обойдется?');   // 5)
+let sum2 = prompt('Во сколько это обойдется?'); // 5)
 
-let budgetMonth = money - sum1;
+/* let budgetMonth = money - sum1;
 console.log('Доход в месяц: ', budgetMonth); // 6)
 
 let mission = 120000;
 console.log(Math.ceil(mission / budgetMonth)); // 7)
 
 budgetMonth = money - sum2;
-console.log(Math.floor(mission / budgetMonth)); // 8)
+console.log(Math.floor(mission / budgetMonth)); // 8) */
+let mission = 120000;
+let budgetMonth = '';
+
+  console.log(Math.ceil(budgetMonth = mission / (money - sum1)));
+  console.log(Math.floor(budgetMonth = mission / (money - sum2)));
 
 
 if (budgetMonth >= 800) {
@@ -35,7 +44,7 @@ if (budgetMonth >= 800) {
   console.log('Низкий уровень дохода');
 } else if (budgetMonth < 0) {
   console.log('Что то пошло не так');
-}                                                 // 9)
+} // 9)
 
-                                                  // 10) Проверено все работает
-                                                  // 11) Добавлено с комитом в ветку мастер
+// 10) Проверено все работает
+// 11) Добавлено с комитом в ветку мастер
